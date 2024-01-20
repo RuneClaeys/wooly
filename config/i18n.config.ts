b886b29d55a4767b5e3fa6fca6693359f6f5de45
Rune Claeys <rune.claeys@ago.jobs>
2024-01-20 17:38:08 +0100

    Fix build errors

--- config/i18n.config.ts
+++ config/i18n.config.ts
@@ -5,7 +5,7 @@ export const i18n: NuxtConfig['i18n'] = {
    langDir: 'i18n',
    strategy: 'no_prefix',
    locales: [
-      { code: 'en', name: 'English' },
-      { code: 'nl', name: 'Nederlands' },
+      { code: 'en', name: 'English', file: 'en.json' },
+      { code: 'nl', name: 'Nederlands', file: 'nl.json' },
    ],
 };
